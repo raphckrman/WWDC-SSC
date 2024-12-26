6f8bd004c382c8f2f776a8428b1eb7965bf7a62b
raphckrman <41128238+raphckrman@users.noreply.github.com>
2024-12-26 17:26:17 +0100

    feat: add settings

--- src/ReCall.swiftpm/Views/Settings/SettingsView.swift
+++ src/ReCall.swiftpm/Views/Settings/SettingsView.swift
@@ -10,7 +10,26 @@ import SwiftUI
 struct SettingsView: View {
     var body: some View {
         BaseView(title: "Settings") {
-
+            List {
+                Section(header: Text("General").offset(x: -15)) {
+                    RowSettingsNavigation(title: "Notifications", icon: "bell.fill", color: Color.accentColor, destination: AnyView(BlankView()))
+                    RowSettingsNavigation(title: "Vibrations", icon: "waveform.path", color: Color.accentColor, destination: AnyView(BlankView()))
+                    RowSettingsNavigation(title: "Daily Goal", icon: "timer", color: Color.accentColor, destination: AnyView(BlankView()))
+                }
+                Section(header: Text("Stats").offset(x: -15)) {
+                    RowSettingsToggle(title: "Study Streak", icon: "flame.fill", color: Color.accentColor)
+                    RowSettingsToggle(title: "Study Hours Tracking", icon: "clock.fill", color: Color.accentColor)
+                    RowSettingsToggle(title: "Progress Overview", icon: "chart.bar.fill", color: Color.accentColor)
+                }
+                Section(header: Text("Resources").offset(x: -15)) {
+                    RowSettingsNavigation(title: "Tutorial", icon: "sparkles.tv.fill", color: Color.accentColor, destination: AnyView(BlankView()))
+                    RowSettingsNavigation(title: "GitHub Repository", icon: "curlybraces.square.fill", color: Color.accentColor, destination: AnyView(BlankView()))
+                    RowSettingsNavigation(title: "Acknowledgments", icon: "person.2.fill", color: Color.accentColor, destination: AnyView(BlankView()))
+                }
+            }
+            .frame(height: 800)
+            .background(.clear)
+            .scrollContentBackground(.hidden)
         }
     }
 }
