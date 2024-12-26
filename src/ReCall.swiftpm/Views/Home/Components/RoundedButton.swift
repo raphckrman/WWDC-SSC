9ef860ceb01ca97770ae028a466c8888cb7f6690
raphckrman <41128238+raphckrman@users.noreply.github.com>
2024-12-26 03:45:45 +0100

    feat: add rounded button component

--- src/ReCall.swiftpm/Views/Home/Components/RoundedButton.swift
+++ src/ReCall.swiftpm/Views/Home/Components/RoundedButton.swift
@@ -5,3 +5,28 @@
 //  Created by Raphaël on 24/12/2024.
 //
 
+import SwiftUI
+
+struct RoundedButton: View {
+    var title: String
+    var width: CGFloat
+    var primary: Bool = true
+    var action: (() -> Void)? = nil
+
+    var body: some View {
+        Button(action: {
+            action?()
+        }) {
+            ZStack {
+                Text(title)
+                    .foregroundColor(primary ? .primary : .primary.opacity(0.6))
+                    .fontWeight(.semibold)
+            }
+            .frame(width: width)
+            .padding()
+            .background(primary ? Color.accentColor : Color.clear)
+            .cornerRadius(100)
+        }
+        .buttonStyle(PressableScaleStyle())
+    }
+}
