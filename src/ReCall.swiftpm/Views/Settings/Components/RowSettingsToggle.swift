ec9beabcaa88f43244f8a02a993906653fa4843f
raphckrman <41128238+raphckrman@users.noreply.github.com>
2024-12-26 17:26:05 +0100

    feat: add a toggle component for settings

---
+++ src/ReCall.swiftpm/Views/Settings/Components/RowSettingsToggle.swift
@@ -0,0 +1,36 @@
+//
+//  RowSettingsToggle.swift
+//  ReCall
+//
+//  Created by Raphaël on 26/12/2024.
+//
+
+import SwiftUI
+
+import SwiftUI
+
+struct RowSettingsToggle: View {
+    var title: String
+    var icon: String
+    var color: Color
+    @State private var isOn: Bool = false
+    var action: ((Bool) -> Void)? = nil
+
+    var body: some View {
+        Toggle(isOn: Binding(
+            get: { isOn },
+            set: { newValue in
+                isOn = newValue
+                action?(newValue)
+            }
+        )) {
+            Label {
+                Text(title)
+                    .foregroundStyle(.primary)
+            } icon: {
+                Image(systemName: icon)
+                    .foregroundStyle(color)
+            }
+        }
+    }
+}
