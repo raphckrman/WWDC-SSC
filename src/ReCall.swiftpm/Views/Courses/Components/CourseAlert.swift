7b8a4faf599d127fdc1f6515e3b156a7c9822852
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-01-14 02:25:02 +0100

    feat(course): first preview of course view

---
+++ src/ReCall.swiftpm/Views/Courses/Components/CourseAlert.swift
@@ -0,0 +1,31 @@
+//
+//  CourseAlert.swift
+//  ReCall
+//
+//  Created by Raphaël on 05/01/2025.
+//
+
+import SwiftUI
+
+struct CourseAlert: View {
+    var icon: String
+    var text: String
+    var color: Color
+    
+    var body: some View {
+        HStack {
+            Image(systemName: icon)
+                .foregroundColor(color)
+                .font(.title)
+            Text(text)
+                .font(.callout)
+                .fontWeight(.semibold)
+                .foregroundColor(color)
+
+        }
+        .padding()
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .background(color.opacity(0.1))
+        .cornerRadius(16)
+    }
+}
--- src/ReCall.swiftpm/Views/Courses/CourseView.swift
+++ src/ReCall.swiftpm/Views/Courses/CourseView.swift
@@ -15,10 +15,47 @@ struct CourseView: View {
     @State private var showCourseAlert = false
 
     var folder: FolderItem
-    
     var body: some View {
         BaseView(title: folder.name) {
-            
+            VStack(alignment: .leading) {
+                VStack(alignment: .leading) {
+                    HStack {
+                        Image(systemName: "clock.fill")
+                        Text("Last time this was reviewed: \(folder.lastReviewedDate.formatted(.dateTime.day().month().year()))")
+                            .font(.body)
+                    }
+                    .opacity(0.7)
+                    .frame(alignment: .leading)
+                    HStack {
+                        Image(systemName: "flame.fill")
+                        Text("Reviewed this course for 3 days in a row!")
+                            .font(.body)
+                    }
+                    .opacity(0.7)
+                    .frame(alignment: .leading)
+                    CourseAlert(icon: "checkmark.seal.fill", text: "Great job! You’ve studied recently, take a break!", color: .green)
+                }
+                .padding(.horizontal)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                //CourseAlert(icon: "exclamationmark.triangle.fill", text: "Your exam is fast approaching, just 3 days to go.", color: .red)
+                    //.padding(.top)
+                //CourseAlert(icon: "clock.fill", text: "You haven't reviewed in a while, it's time to get back on track!", color: .orange)
+                
+                sectionTitle(title: "Flashcards") {
+                } toolbarContent: {
+                    AnyView(
+                        HStack {
+                            Button("", systemImage: "plus.circle") {
+                                print("ok")
+                            }                        }
+                    )
+                }
+                sectionTitle(title: "Improvements") {
+                }
+                sectionTitle(title: "Badges") {
+                }
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
         } toolbarContent: {
             AnyView(
                 HStack {
